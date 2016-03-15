91ded2c12dbee201337a4e206d8fa32b6e6ea671
Allen Brooks <c.allenbrooks@gmail.com>
2016-03-15 17:30:35 -0400

    First commit

---
+++ ImageResizer_V2.0.03152016/Options.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+
+namespace ImageResizer_V2._0._03152016
+{
+    class Options
+    {
+        [Option('m', "make-directory", Required = false, HelpText = "Creates the image directory if it doesn't exist.")]
+        public bool CreateDirectory { get; set; }
+
+        [Option('a', "all-files", Required = false, HelpText = "Processes files more than 30 days old (default: false)")]
+        public bool ProcessOldFiles { get; set; }
+
+        [Option('p', "pdf", Required = false, HelpText = "Processes PDFs when encountered (default: false")]
+        public bool ProcessPDFs { get; set; }
+
+        // TODO: Set default value to the default directory
+        [Option('d', "directory", Required = true, HelpText = "Specifies the directory from which to process the images.")]
+        public string Directory { get; set; }
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            var help = new HelpText
+            {
+                Heading = new HeadingInfo("<>", "<>"),
+                Copyright = new CopyrightInfo("<>", DateTime.Now.Year),
+                AdditionalNewLineAfterOption = true,
+                AddDashesToOption = true
+            };
+
+            help.AddPreOptionsLine("<>");
+            help.AddPreOptionsLine("Usage: ImageResizer -m");
+            help.AddOptions(this);
+            return help;
+        }
+    }
+}
---
+++ ImageResizer_V2.0.03152016/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace ImageResizer_V2._0._03152016
+{
+    class Program
+    {
+        private static Options options;
+        private static Parser parser;
+
+        private static string ServerName;
+        private static string LocationCode;
+        private static string Frim;
+        private static string ImageSourcePath;
+        private static string ImageDestinationPath;
+        private static string ACSJobNumber;
+        private static int MaxPictureSize;
+        private static DateTime StartTime;
+        static void Main(string[] args)
+        {
+            options = new Options();
+            parser = new Parser();
+
+            // TODO: Set default arguments
+
+            if(parser.ParseArguments(args, options))
+            {
+
+            }
+        }
+    }
+}
