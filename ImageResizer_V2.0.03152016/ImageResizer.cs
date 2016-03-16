09b299e06a15acf26d620f66398509013b1aac3c
Allen Brooks <c.allenbrooks@gmail.com>
2016-03-16 08:09:49 -0400

    Utilities and ImageResizer add

---
+++ ImageResizer_V2.0.03152016/ImageResizer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+
+namespace ImageResizer_V2._0._03152016
+{
+    class ImageResizer
+    {
+
+        public static Image Resize(Image image, Size size)
+        {
+
+            try
+            {
+                int sourceWidth = image.Width;
+                int sourceHeight = image.Height;
+
+                float WidthScaling = ((float)size.Width / (float)sourceWidth);
+                float HeightScaling = ((float)size.Height / (float)sourceHeight);
+                float ScalingValue = HeightScaling < WidthScaling ? HeightScaling : WidthScaling;
+
+                int destinationWidth = (int)(sourceWidth * ScalingValue);
+                int destinationHeight = (int)(sourceHeight * ScalingValue);
+
+                Bitmap bitmap = new Bitmap(destinationWidth, destinationHeight);
+                Graphics graphics = Graphics.FromImage((Image)bitmap);
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.DrawImage(image, 0, 0, destinationWidth, destinationHeight);
+                graphics.Dispose();
+                return (Image)bitmap;
+            }
+            catch (Exception ex)
+            {
+                ("Image resize failed. Moving to next file.").WriteLine();
+                Log.WriteToLog("Image resizing failed: " + ex.Message);
+                Log.WriteToEventLog("Image resizing failed: " + ex.Message, EventLogEntryType.Error);
+                return null;
+            }
+        }
+    }
+}
---
+++ ImageResizer_V2.0.03152016/Utilities.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageResizer_V2._0._03152016
+{
+    public static class Utilities
+    {
+        public static void WriteLine(this string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void Write(this string message)
+        {
+            Console.Write(message);
+        }
+
+        public static void PressAnyKeyToExit()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        public static bool CheckDependencies()
+        {
+            try
+            {
+                return new Ping().Send("schintranet").Status == IPStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                Log.WriteToLog("Ping error: " + ex.InnerException.Message);
+                return false;
+            }
+        }
+    }
+}
